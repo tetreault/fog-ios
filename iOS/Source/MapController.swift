70f6ab0e9436d7170303be9673358953b49a842e
Igor Ranieri Elland <igor@elland.me>
2016-10-13 14:44:33 +0200

    Add map view and location tracking

--- iOS/Source/MapController.swift
+++ iOS/Source/MapController.swift
@@ -1,7 +1,30 @@
-import UIKit
+import SweetUIKit
+import CoreLocation
+import MapKit
+
+fileprivate let userPinReuseIdentifier = "userPin"
 
 class MapController: UIViewController {
     var fetcher: Fetcher
+
+    var latestCoordinate: CLLocationCoordinate2D?
+
+
+    lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.desiredAccuracy = 25.0 // meters
+
+        return manager
+    }()
+
+    lazy var mapView: MKMapView = {
+        let view = MKMapView(withAutoLayout: true)
+        view.showsUserLocation = true
+
+        return view
+    }()
 
     public init(fetcher: Fetcher) {
         self.fetcher = fetcher
@@ -15,5 +38,19 @@ class MapController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.locationManager.startUpdatingLocation()
+        
+        self.view.addSubview(self.mapView)
+        self.mapView.fillSuperview()
+    }
+}
+
+extension MapController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+            self.mapView.setRegion(region, animated: true)
+        }
     }
 }
